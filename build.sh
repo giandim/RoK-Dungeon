09a1d3d34009eaf651ff714bb582787b4e49bf73
gianluca <gianlucad@raininginc.com>
2024-02-21 20:57:22 -0500

    update the build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-GOOS=js GOARCH=wasm go build -o dist/editor.wasm game-engine/editor.go game-engine/utils.go
+GOOS=js GOARCH=wasm go build -o dist/editor.wasm game-engine/*.go
 goexec 'http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))'
